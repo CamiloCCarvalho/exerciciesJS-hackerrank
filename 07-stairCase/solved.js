4a6a09680f0b1be346ca86e547d10df3adad4ca6
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-06-13 09:04:42 -0300

    Commit Att

--- 07-stairCase/solved.js
+++ 07-stairCase/solved.js
@@ -29,9 +29,16 @@ function readLine() {
 // Ignore de code above, it's used to run at site HackerRank
 
 function staircase(n) {
-    
-    
-
+    for(let i=0; i< n; i++){
+        let string = ''
+        for(let j=0; j < n-i-1; j++){
+            string += ' '
+        }
+        for(let k =0; k < i+1; k++){
+            string += '#'
+        }
+        console.log(string)
+    }
 }
 
 // Ignore de code above, it's used to run at site HackerRank
--- test.js
+++
@@ -1,3 +0,0 @@
-let tex = " "
-
-return tex
