aae459e54d8944d6eb4398de95a16cb99ca80d50
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-06-15 14:26:25 -0300

    Commit Att

--- 11-divisibleSumPairs/solved.js
+++ 11-divisibleSumPairs/solved.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 
 process.stdin.resume();
@@ -8,18 +6,18 @@ process.stdin.setEncoding('utf-8');
 let inputString = '';
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on('end', () => {
+  inputString = inputString.split('\n');
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -34,33 +32,33 @@ function readLine() {
 // Ignore the code above, it's used to run at site HackerRank
 
 function divisibleSumPairs(n, k, ar) {
-    let res = 0
-    for(let i=0; i < ar.length; i++){
-        for(let j = i+1; j < ar.length; j++){
-            if( (ar[i] + ar[j]) % k == 0){
-                res++
-            }
-        }
+  let res = 0;
+  for (let i = 0; i < ar.length; i++) {
+    for (let j = i + 1; j < ar.length; j++) {
+      if ((ar[i] + ar[j]) % k == 0) {
+        // eslint-disable-next-line no-plusplus
+        res++
+      }
     }
-    return res
-
+  }
+  return res
 }
 
 // Ignore the code below, it's used to run at site HackerRank
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+  const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
 
-    const n = parseInt(firstMultipleInput[0], 10);
+  const n = parseInt(firstMultipleInput[0], 10);
 
-    const k = parseInt(firstMultipleInput[1], 10);
+  const k = parseInt(firstMultipleInput[1], 10);
 
-    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+  const ar = readLine().replace(/\s+$/g, '').split(' ').map((arTemp) => parseInt(arTemp, 10));
 
-    const result = divisibleSumPairs(n, k, ar);
+  const result = divisibleSumPairs(n, k, ar);
 
-    ws.write(result + '\n');
+  ws.write(`${result}\n`);
 
-    ws.end();
+  ws.end();
 }
