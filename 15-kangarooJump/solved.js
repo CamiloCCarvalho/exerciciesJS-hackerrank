e2d990a2266b39930af4f2953d8eefad05d0766f
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-07-17 19:58:08 -0300

    Commit Att

--- 15-kangarooJump/solved.js
+++ 15-kangarooJump/solved.js
@@ -1,4 +1,26 @@
-<?php
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
 
 /*
  * Complete the 'kangaroo' function below.
@@ -11,49 +33,29 @@
  *  4. INTEGER v2
  */
 
-function kangaroo($x1, $v1, $x2, $v2) {
+// Igonre the code above, it's used to run at site hackerrank
+function kangaroo(x1, v1, x2, v2) {
     
-    $pk1 = $x1 + $v1;
-    $pk2 = $x2 + $v2;
-    $firstGap = null;
-    $secondGap = null;
-    
-    if($pk1 > $pk2){
-        $firstGap = $pk1-$pk2;
-        $secondGap = ($pk1 += $v1) - ($pk2 += $v2);
-    }else{
-        $firstGap = $pk2-$pk1;
-        $secondGap = ($pk2 += $v2) - ($pk1 += $v1);
-    }
-    
-    if($secondGap >= $firstGap){
-        return "NO";
-    }else{
-        $t = $secondGap - $firstGap;
-        if($secondGap%$t == 0){
-            return "YES";
-        }else{
-            return "NO";
-        }
-    }
+    // Write your code here
+
 }
+// Igonre the code below, it's used to run at site hackerrank
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-// Ignore the code below, it's used to run at HackerHank site
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
 
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+    const x1 = parseInt(firstMultipleInput[0], 10);
 
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
+    const v1 = parseInt(firstMultipleInput[1], 10);
 
-$x1 = intval($first_multiple_input[0]);
+    const x2 = parseInt(firstMultipleInput[2], 10);
 
-$v1 = intval($first_multiple_input[1]);
+    const v2 = parseInt(firstMultipleInput[3], 10);
 
-$x2 = intval($first_multiple_input[2]);
+    const result = kangaroo(x1, v1, x2, v2);
 
-$v2 = intval($first_multiple_input[3]);
+    ws.write(result + '\n');
 
-$result = kangaroo($x1, $v1, $x2, $v2);
-
-fwrite($fptr, $result . "\n");
-
-fclose($fptr);
+    ws.end();
+}
--- 15-kangarooJump/try.js
+++ 15-kangarooJump/try.js
@@ -1,4 +1,26 @@
-<?php
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
 
 /*
  * Complete the 'kangaroo' function below.
@@ -11,27 +33,29 @@
  *  4. INTEGER v2
  */
 
-function kangaroo($x1, $v1, $x2, $v2) {
+// Igonre the code above, it's used to run at site hackerrank
+function kangaroo(x1, v1, x2, v2) {
     
     // Write your code here
+
 }
+// Igonre the code below, it's used to run at site hackerrank
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-// Ignore the code below, it's used to run at HackerHank site
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
 
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+    const x1 = parseInt(firstMultipleInput[0], 10);
 
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
+    const v1 = parseInt(firstMultipleInput[1], 10);
 
-$x1 = intval($first_multiple_input[0]);
+    const x2 = parseInt(firstMultipleInput[2], 10);
 
-$v1 = intval($first_multiple_input[1]);
+    const v2 = parseInt(firstMultipleInput[3], 10);
 
-$x2 = intval($first_multiple_input[2]);
+    const result = kangaroo(x1, v1, x2, v2);
 
-$v2 = intval($first_multiple_input[3]);
+    ws.write(result + '\n');
 
-$result = kangaroo($x1, $v1, $x2, $v2);
-
-fwrite($fptr, $result . "\n");
-
-fclose($fptr);
+    ws.end();
+}
