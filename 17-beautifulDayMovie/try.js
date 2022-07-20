d8dc8f30b228c07b55807a281cdca8322111629d
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-07-20 04:31:21 -0300

    Commit Att

--- 17-beautifulDayMovie/try.js
+++ 17-beautifulDayMovie/try.js
@@ -31,15 +31,13 @@ function readLine() {
  *  2. INTEGER j
  *  3. INTEGER k
  */
-
+// Ignore the code above, it's used to run at site hackerrank
 function beautifulDays(i, j, k) {
-    let days = 0
-    for(i; i<=j; i++){
-        let numInverse = Number(i.toString().split("").reverse().join(""))
-        days += ((i - numInverse)%k == 0) ? 1:0}
-    return days
-}
 
+    // Write your code here
+    
+}
+// Ignore the code below, it's used to run at site hackerrank
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
